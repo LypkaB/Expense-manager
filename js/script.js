86429e66a4e94ed9a918035f69e432142d04a811
LypkaB <lpk.bdn@gmail.com>
2019-10-02 13:07:00 +0300

    Added all functions in main object

--- js/script.js
+++ js/script.js
@@ -18,60 +18,69 @@ let appData = {
     expenses: {},
     optionalExpenses: {},
     income: [],
-    savings: true
+    savings: true,
+    chooseExpenses: function () {
+        for (let i = 0; i < 2; i++) {
+            let a = prompt('Enter a required cost item this month', ''),
+                b = prompt('How much will it cost?', '');
+
+            if ( (typeof (a)) === 'string' && (typeof (a)) != null  && (typeof (b)) != null
+                && a != '' && b != '' && a.length < 50) {
+                console.log('done');
+                appData.expenses[a] = b;
+            } else {
+                i = i - 1;
+            }
+        }
+    },
+    detectDayBudget: function () {
+        appData.moneyPerDay = (appData.budget / 30).toFixed();
+        alert ('1 day budget ' + appData.moneyPerDay + '$');
+    },
+    detectLevel: function () {
+        if (appData.moneyPerDay < 100) {
+            console.log('This is the minimum level of prosperity!');
+        } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+            console.log('This is an average level of prosperity!');
+        } else if (appData.moneyPerDay > 2000) {
+            console.log('This is a high level of prosperity!');
+        } else {
+            console.log('Mistake...!');
+        }
+    },
+    checkSavings: function () {
+        if (appData.savings == true) {
+            let save = +prompt('What is the accumulation?');
+            let percent = +prompt('What percentage?');
+
+            appData.monthIncome = save / 100 / 12 * percent;
+            alert('Income per month from your deposit: ' + appData.monthIncome);
+        }
+    },
+    chooseOptExpenses: function () {
+        for (let i = 1; i <= 3; i++) {
+            let questionOptExpenses = prompt('An item of optional expenses?');
+            appData.optionalExpenses[i] = questionOptExpenses;
+            console.log(appData.optionalExpenses);
+        }
+    },
+    chooseIncome: function () {
+        let items = prompt('What will bring additional income? (List with a comma)', '');
+
+        if (typeof(items) != 'string' || items == '' || typeof(items) == null) {
+            console.log('You entered incorrect data or did not enter it at all');
+        } else {
+            appData.income = items.split(', ');
+            appData.income.push(prompt('Maybe something else?'));
+            appData.income.sort();
+        }
+
+        appData.income.forEach (function (itemmassive, i) {
+            alert('Additional methods earnings: ' + (i+1) + ' - ' + itemmassive);
+        });
+    }
 };
 
-function chooseExpenses() {
-    for (let i = 0; i < 2; i++) {
-        let a = prompt('Enter a required cost item this month', ''),
-            b = prompt('How much will it cost?', '');
-
-        if ( (typeof (a)) === 'string' && (typeof (a)) != null  && (typeof (b)) != null
-            && a != '' && b != '' && a.length < 50) {
-            console.log('done');
-            appData.expenses[a] = b;
-        } else {
-            i = i - 1;
-        }
-    }
+for (let key in appData) {
+    console.log('Our program includes data: ' + key + ' - ' + appData[key]);
 }
-chooseExpenses();
-
-function detectDayBudget() {
-    appData.moneyPerDay = (appData.budget / 30).toFixed();
-    alert ('1 day budget ' + appData.moneyPerDay + '$');
-}
-detectDayBudget();
-
-function detectLevel() {
-    if (appData.moneyPerDay < 100) {
-        console.log ('This is the minimum level of prosperity!');
-    } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-        console.log ('This is an average level of prosperity!');
-    } else if (appData.moneyPerDay > 2000) {
-        console.log ('This is a high level of prosperity!');
-    } else {
-        console.log ('Mistake...!');
-    }
-}
-detectLevel();
-
-function checkSavings() {
-    if (appData.savings == true) {
-        let save = +prompt("What's the accumulation?");
-        let percent = +prompt('What percentage?');
-
-        appData.monthIncome = save/100/12*percent;
-        alert('Income per month from your deposit: ' + appData.monthIncome);
-    }
-}
-checkSavings();
-
-function chooseOptExpenses() {
-    for (let i = 1; i <= 3; i++) {
-        let questionOptExpenses = prompt('An item of optional expenses?');
-        appData.optionalExpenses[i] = questionOptExpenses;
-        console.log(appData.optionalExpenses);
-    }
-}
-chooseOptExpenses();

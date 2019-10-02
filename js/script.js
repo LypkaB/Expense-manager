7f205b5a4da473036ec5637a6d791cc7251dea14
LypkaB <lpk.bdn@gmail.com>
2019-10-02 08:12:32 +0300

    Added cycle for repeat question

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
-let money = prompt('What is your monthly budget?'),
-    time = prompt('Enter the date in the format YYYY-MM-DD');
+let money = +prompt("What's your monthly budget?", ''),
+    time = prompt('Enter the date in the format YYYY-MM-DD', '');
 
 let appData = {
     budget: money,
@@ -12,12 +12,30 @@ let appData = {
     savings: false
 };
 
-let a1 = prompt('Enter a required cost item this month', ''),
-    a2 = prompt('How much will it cost?', ''),
-    a3 = prompt('Enter a required cost item this month', ''),
-    a4 = prompt('How much will it cost?', '');
+for (let i = 0; i < 2; i++) {
+    let a = prompt('Enter a required cost item this month', ''),
+        b = prompt('How much will it cost?', '');
 
-appData.expenses.a1 = a2;
-appData.expenses.a3 = a4;
+    if ( (typeof (a)) === 'string' && (typeof (a)) != null  && (typeof (b)) != null
+        && a != '' && b != '' && a.length < 50) {
+        console.log('Done');
+        appData.expenses[a] = b;
+    } else {
+        console.log('Bad result');
+        i--;
+    }
+}
 
-alert(appData.budget / 30);
+appData.moneyPerDay = appData.budget / 30;
+
+alert('Daily budget: ' + appData.moneyPerDay);
+
+if(appData.moneyPerDay < 100) {
+    console.log('Minimum level of wealth');
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log('Average income');
+} else if (appData.moneyPerDay > 2000) {
+    console.log('High level of wealth');
+} else {
+    console.log('An error has occurred');
+}
